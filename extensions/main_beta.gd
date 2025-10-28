0176dc0fed6eae0e7eb443fe7efded7d864ca9b5
JonathanFox <ylx3434@gmail.com>
2025-10-28 10:17:47 +0800

    补充角色和道具图标 修复官方铁砧问题导致面具先薛定谔的猫再善战者可能会稳定扣%伤害

--- extensions/main_beta.gd
+++ extensions/main_beta.gd
@@ -10,3 +10,12 @@ func _on_enemy_took_damage(enemy: Enemy, _value: int, _knockback_direction: Vect
 	if enemy.dead and _is_crit and args.hitbox.from is Structure :
 		for effect in RunData.get_player_effect("temp_stats_on_structure_crit", args.from_player_index):
 			TempStats.add_stat(effect[0], effect[1], args.from_player_index)
+
+func manage_harvesting() -> void :
+	for player_index in RunData.get_player_count():
+		# 官方代码纰漏：铁砧升级的时候没有用get_player_effect_bool("lock_current_weapons")，导致负数也被认为是true
+		# 薛定谔的猫可能会有负数的lock_current_weapons，会让善战者自带的铁砧总是-100%伤害
+		var effects = RunData.get_player_effects(player_index)
+		if effects["lock_current_weapons"] < 0:
+			effects["lock_current_weapons"] = 0
+	.manage_harvesting()

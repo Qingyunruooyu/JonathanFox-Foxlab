e46017ba77e0965defb454f4d61a9250706bd849
JonathanFox <ylx3434@gmail.com>
2025-12-03 10:33:10 +0800

    优化追踪器 新角色技术宅

--- contents/effects/tracker_effect.gd
+++ contents/effects/tracker_effect.gd
@@ -1,8 +1,26 @@
 class_name FoxLabTrackerEffect
-extends FoxLabTurretEffect
+extends  TurretEffect
+
+var tracker = preload("res://mods-unpacked/JonathanFox-FoxLab/contents/entities/structures/turret/tracker/tracker.gd")
+
+static func get_id() -> String:
+	return "foxlab_effect_tracker"
 
 func get_args(player_index: int) -> Array:
 	var args = .get_args(player_index)
-	var max_range = FoxLabTracker.get_max_range_melee_weapon_range(stats, player_index)
+	var max_range = tracker.get_max_range_melee_weapon_range(stats, player_index)
 	args.push_back(str(max_range))
 	return args
+
+func unapply(player_index: int) -> void :
+	var effects = RunData.get_player_effects(player_index)["structures"]
+	var num =  effects.size()
+	.unapply(player_index)
+	var num_after =  effects.size()
+	if num_after < num:
+		return
+	for effect in effects:
+		if effect.get_id() == get_id() and effect.text_key == text_key and\
+			 effect.get_args(player_index) == get_args(player_index):
+			effects.erase(effect)
+			return
--- contents/entities/structures/turret/tracker/tracker.gd
+++ contents/entities/structures/turret/tracker/tracker.gd
@@ -1,25 +1,45 @@
 class_name FoxLabTracker
 extends Turret
 
-var distance = rand_range(90, 100)
-var rotation_speed = rand_range(2, 2.5)
+var move_speed = rand_range(300, 350)
+var _orbit_radius = rand_range(90, 100)
+var _orbit_speed = rand_range(2, 2.5)
+var _orbit_angle: float = 0.0
+var _current_orbit_center: Vector2 = ZoneService.get_map_center()
 
 var _current_tracking_enemy: Enemy = null
-var _position: Vector2 = ZoneService.get_map_center()
+var _player_tracked: = false
+
 var _angle = rand_range(0, 2 * PI)
+var _players: = []
+const _PLAYER_TRACKING_DISTANCE = 50.0
 
 export (Resource) var slow_sound
+
+func init(zone_min_pos: Vector2, zone_max_pos: Vector2, players_ref: Array = [], _entity_spawner_ref = null) -> void :
+	.init(zone_min_pos, zone_max_pos, players_ref, _entity_spawner_ref)
+	_players = players_ref
+	if Utils.get_chance_success(0.5):
+		_orbit_speed = -_orbit_speed
 
 static func get_max_range_melee_weapon_range(stats: Resource, player_index:int) -> int:
 	var player_weapons = RunData.get_player_weapons(player_index)
 	var max_range = 0
-	var has_melee_weapon = false
+	var best_weapon:WeaponData = null
 	for weapon in player_weapons:
 		if weapon.type == WeaponType.RANGED:
 			continue
-		has_melee_weapon = true
-		max_range = max(max_range, weapon.stats.max_range)
-	return max_range + Utils.get_stat("stat_range", player_index) / 2 if has_melee_weapon else stats.max_range
+		if weapon.stats.max_range > max_range:
+			best_weapon = weapon
+			max_range = weapon.stats.max_range
+	if best_weapon:
+		var args := WeaponServiceInitStatsArgs.new()
+		args.sets = best_weapon.sets
+		args.effects = best_weapon.effects
+		var current_stats = WeaponService.init_melee_stats(best_weapon.stats, player_index, args)
+		max_range = current_stats.max_range
+
+	return max_range if best_weapon else stats.max_range as int
 
 func set_data(data: Resource) -> void :
 	var updated_data = data.duplicate()
@@ -43,8 +63,7 @@ func _physics_process(delta: float) -> void :
 		for enemy in _targets_in_range:
 			if not enemy is Enemy:
 				continue
-			var distance = global_position.distance_to(enemy.global_position)
-			if distance > stats.max_range:
+			if global_position.distance_to(enemy.global_position) > stats.max_range:
 				continue
 			if enemy is Boss:
 				if min_hp_boss == null or enemy.current_stats.health < min_boss_health:
@@ -62,11 +81,32 @@ func _physics_process(delta: float) -> void :
 		if _current_tracking_enemy:
 			var _error = _current_tracking_enemy.connect("died", self, "on_tracking_enemy_died")
 
+	var new_center = _current_orbit_center
 	if is_instance_valid(_current_tracking_enemy) and not _current_tracking_enemy.dead:
-		_position =  _current_tracking_enemy.global_position
+		new_center =  _current_tracking_enemy.global_position
+		_player_tracked = false
+	elif _player_tracked or global_position.distance_to(_players[player_index].global_position) <= _PLAYER_TRACKING_DISTANCE:
+		_player_tracked = true
+		new_center = _players[player_index].global_position
 
-	_angle += delta * rotation_speed
-	global_position = Vector2(_position.x + cos(_angle) * distance, _position.y + sin(_angle) * distance)
+	# 如果旋转中心改变了，重新计算当前角度
+	if _current_orbit_center != new_center:
+		# 计算当前相对于旧中心的向量和角度
+		var current_offset = global_position - _current_orbit_center
+		_orbit_angle = current_offset.angle()
+
+	# 更新旋转中心（无论是移动还是旋转都需要更新）
+	_current_orbit_center = new_center
+
+	# 无论旋转中心是否移动，都要增加轨道角度
+	_orbit_angle += _orbit_speed * delta
+
+	# 计算新的位置
+	var new_offset = Vector2.RIGHT.rotated(_orbit_angle) * _orbit_radius
+	var desired_position = _current_orbit_center + new_offset
+
+	# 平滑移动到新位置
+	global_position = global_position.move_toward(desired_position, move_speed * delta)
 
 
 func _on_SlowHitbox_hit_something(thing_hit: Node, _damage_dealt: int) -> void :

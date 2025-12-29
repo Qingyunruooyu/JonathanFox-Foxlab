8f2a042cf1473f0e0f641a692f18309c73d80725
JonathanFox <ylx3434@gmail.com>
2025-12-30 00:10:28 +0800

    提高道具计数功能兼容性

--- extensions/item_description.gd
+++ extensions/item_description.gd
@@ -2,10 +2,11 @@ extends "res://ui/menus/shop/item_description.gd"
 
 func set_item(item_data: ItemParentData, player_index: int, item_count: = 1) -> void :
 	.set_item(item_data, player_index, item_count)
-	if _category.text == tr("ITEM"):
-		var number = RunData.get_nb_item(item_data.my_id, player_index);
-		_category.text += "(%s/∞)" % [str(number)]
-	elif _category.text == tr("UNIQUE"):
-		var number = RunData.get_nb_item(item_data.my_id, player_index);
-		if number > 1:
-			_category.text += "(%s/1)" % [str(number)]
+	if item_data is ItemData and not item_data is UpgradeData and not item_data is DifficultyData and not item_data is CharacterData:
+		if item_data.max_nb <= 0:
+			var number = RunData.get_nb_item(item_data.my_id, player_index);
+			_category.text += "(%s/∞)" % [str(number)]
+		elif item_data.max_nb == 1:
+			var number = RunData.get_nb_item(item_data.my_id, player_index);
+			if number > 1:
+				_category.text += "(%s/1)" % [str(number)]

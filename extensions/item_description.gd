bee047415447e7e3fe041d3dee8d02d67485d655
JonathanFox <ylx3434@gmail.com>
2026-03-03 00:04:47 +0800

    武器描述太长时，打开滚动条以免无法合成/回收等操作 调整龟龟（删了十字弓、琴等没什么配合的）、赏金猎人初始武器（加了扳手）

--- extensions/item_description.gd
+++ extensions/item_description.gd
@@ -1,6 +1,19 @@
 extends "res://ui/menus/shop/item_description.gd"
 
+var foxlab_expand_indefinitely = true
+
+func _ready() -> void :
+	foxlab_expand_indefinitely = expand_indefinitely
+
 func set_item(item_data: ItemParentData, player_index: int, item_count: int = 1)->void :
+	if item_data is WeaponData and item_data.effects.size() >= 15:
+		expand_indefinitely = false
+	else:
+		expand_indefinitely = foxlab_expand_indefinitely
+
+	_vbox_container.visible = show_details and expand_indefinitely
+	_scroll_container.visible = show_details and not expand_indefinitely
+
 	.set_item(item_data, player_index, item_count)
 	if item_data is ItemData and not item_data is CharacterData and not item_data is UpgradeData and not item_data is DifficultyData:
 		if item_data.max_nb <= 0:
@@ -10,3 +23,4 @@ func set_item(item_data: ItemParentData, player_index: int, item_count: int = 1)
 			var number = RunData.get_nb_item(item_data.my_id_hash, player_index);
 			if number > 1:
 				_category.text += "(%s/1)" % [str(number)]
+

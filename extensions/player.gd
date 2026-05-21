24c75113e0eafd31a47bd308047d7217563d2fe7
JonathanFox <ylx3434@gmail.com>
2026-05-21 10:03:51 +0800

    提高水母盾改动的兼容性

--- extensions/player.gd
+++ extensions/player.gd
@@ -43,6 +43,24 @@ func _ready() -> void :
 
 	_foxlab_has_charmed_all = RunData.get_player_effect(Utils.foxlab_charm_all_when_fully_heal_hash, player_index).empty()
 
+	call_deferred("foxlab_connect_jelly_shields")
+
+func foxlab_connect_jelly_shields():
+	for jelly in jellyshields:
+		var _err = jelly.get_node("Hurtbox").connect("area_entered", self, "_on_foxlab_jellyshield_Hurtbox_area_entered", [jelly])
+
+func _on_foxlab_jellyshield_Hurtbox_area_entered(hitbox: Area2D, jelly: Node2D) -> void :
+	var parent = hitbox.get_parent()
+	# 是玩家投射物但是却是敌人投射物的掩码，会攻击玩家，但又不是治疗
+	if parent is PlayerProjectile and (hitbox.collision_layer & Utils.ENEMY_PROJECTILES_BIT) and not parent._weapon_stats.is_healing:
+		RunData.add_tracked_value(player_index, Keys.item_jellyshield_hash, 1)
+		# 为什么不用hitbox.hit_something(self, 0)？
+		# 因为JellyFish是Entity，不是Unit，玩家投射物会对thing_hit做add_deacying_speed（鱼叉枪等）、._entity_spawner_ref（反弹）等操作，Entity不支持
+		parent.stop()
+		jelly._animation_player.play("hit")
+		yield(jelly._animation_player, "animation_finished")
+		jelly._animation_player.play("idle")
+
 func _foxlab_has_curse():
 	if RunData.get_player_character(player_index).is_cursed:
 		return true

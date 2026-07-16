d628a36635ae21367fb7d40ff8bbf54bc1f09fdd
JonathanFox <ylx3434@gmail.com>
2026-07-16 21:50:28 +0800

    旱魃选角色界面增加燃烧效果

--- extensions/character_panel_ui.gd
+++ extensions/character_panel_ui.gd
@@ -2,8 +2,13 @@ extends "res://ui/menus/ingame/character_panel_ui.gd"
 
 var foxlab_potato_texture = load("res://entities/units/player/potato.png")
 var foxlab_transparent_texture = load("res://mods-unpacked/JonathanFox-FoxLab/contents/enemy_icons/transparent_icon.png")
+var foxlab_burning_particles = null
 
 func apply_items_appearance(all_items: Array) -> void :
+	if foxlab_burning_particles:
+		foxlab_burning_particles.emitting = false
+		foxlab_burning_particles.visible = false
+
 	if RunData.get_player_character(player_index) and \
 		RunData.get_player_character(player_index).my_id_hash == Utils.character_foxlab_faceless_hash:
 		all_items = all_items.duplicate()
@@ -17,12 +22,31 @@ func apply_items_appearance(all_items: Array) -> void :
 		else:
 			all_items.append({"item_appearances": RunData.get_player_appearances(player_index)})
 
+
 	var potato = $"%Character"/Sprite
 	potato.texture = foxlab_potato_texture
 	var legs = $"%Character"/Legs
 	legs.visible = true
 
 	.apply_items_appearance(all_items)
+
+
+	var add_burning_particle = false
+	if RunData.get_player_effect_bool(Utils.foxlab_burning_proof_hash, player_index):
+		add_burning_particle = true
+	else:
+		for item in all_items:
+			if item.get("my_id_hash") == Utils.character_foxlab_nyuba_hash:
+				add_burning_particle = true
+	if add_burning_particle:
+		var character_node = $"%Character"
+		if foxlab_burning_particles == null:
+			foxlab_burning_particles = load("res://particles/burning/unit_burning_particles.tscn").instance()
+			character_node.add_child(foxlab_burning_particles)
+			foxlab_burning_particles.position = Vector2( 0, -24 )
+		foxlab_burning_particles.emitting = true
+		foxlab_burning_particles.visible = true
+		character_node.move_child(foxlab_burning_particles, character_node.get_child_count() - 1)
 
 	for item in all_items:
 		for app in item.item_appearances:

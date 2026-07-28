41293e562eafa287301effcd6ec356b22b3c6f08
Jonathan Fox <ylx3434@gmail.com>
2026-07-28 10:19:25 +0800

    调整物品描述折叠的多MOD兼容性（任意一个MOD想要折叠，都会折叠）

--- extensions/item_description.gd
+++ extensions/item_description.gd
@@ -1,25 +1,35 @@
 extends "res://ui/menus/shop/item_description.gd"
 
-var foxlab_expand_indefinitely = true
+var foxlab_item_is_special_upgrade = false
 
-func _ready() -> void :
-	foxlab_expand_indefinitely = expand_indefinitely
+func foxlab_should_scroll() -> bool:
+	return (RunData.shop_effects_checked and item is WeaponData and item.effects.size() > 9)\
+		or (not RunData.is_coop_run and foxlab_item_is_special_upgrade)
 
-func set_item(item_data: ItemParentData, player_index: int, item_count: int = 1)->void :
-	var is_special_upgrade = item_data is UpgradeData and not Utils.foxlab_is_vanilla_upgrade(item_data)
-	if is_special_upgrade:
-		item_data = item_data.get_meta("foxlab_item", item_data)
-
-	if (RunData.shop_effects_checked and item_data is WeaponData and item_data.effects.size() > 9)\
-		or (not RunData.is_coop_run and is_special_upgrade):
-		expand_indefinitely = false
-	else:
-		expand_indefinitely = foxlab_expand_indefinitely
-
+func foxlab_update_visibility() -> void:
+	expand_indefinitely = (get_effects() == _effects)
 	_vbox_container.visible = show_details and expand_indefinitely
 	_scroll_container.visible = show_details and not expand_indefinitely
 
+########## 扩展 ############
+func get_weapon_stats():
+	if .get_weapon_stats() == _weapon_stats_scrolled || foxlab_should_scroll():
+		return _weapon_stats_scrolled
+	return _weapon_stats
+
+func get_effects():
+	if .get_effects() == _effects_scrolled || foxlab_should_scroll():
+		return _effects_scrolled
+	return _effects
+
+func set_item(item_data: ItemParentData, player_index: int, item_count: int = 1)->void :
+	foxlab_item_is_special_upgrade = item_data is UpgradeData and not Utils.foxlab_is_vanilla_upgrade(item_data)
+	if foxlab_item_is_special_upgrade:
+		item_data = item_data.get_meta("foxlab_item", item_data)
+
 	.set_item(item_data, player_index, item_count)
+
+	foxlab_update_visibility()
 
 	if item_data is ItemData and not item_data is CharacterData and not item_data is UpgradeData and not item_data is DifficultyData:
 		if item_data.max_nb <= 0:
